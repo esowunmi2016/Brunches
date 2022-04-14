a043c984f324abf9c17bea02008eb1a24b18c021
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-14 14:43:13 +0100

    host test 1

--- src/components/landing1.js
+++ src/components/landing1.js
@@ -1,19 +1,19 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+// import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-function Landing1() {
-  return ( 
-    <div
-      style={{
-        width:'100vw',
-        height:'100vh',
-        background:'green'
-      }}
-    >
-      <h2>
-        <Link to="/">Home</Link>
-      </h2>
-    </div>
-   );
-}
+// function Landing1() {
+//   return ( 
+//     <div
+//       style={{
+//         width:'100vw',
+//         height:'100vh',
+//         background:'green'
+//       }}
+//     >
+//       <h2>
+//         <Link to="/">Home</Link>
+//       </h2>
+//     </div>
+//    );
+// }
 
-export default Landing1;
+// export default Landing1;
