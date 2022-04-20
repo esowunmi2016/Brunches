b97b18722fce9c8d4417182eb5a7ff58b73483ad
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-20 14:01:28 +0100

    .

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,5 +1,6 @@
-import { Carousel } from 'antd';
+import { Carousel, Spin } from 'antd';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { useState, useEffect } from 'react';
 
 
 import img1 from '../images/8.jpeg'
@@ -14,68 +15,87 @@ const contentStyle = {
 
 
 function Carousel1() {
+
+  const [loading, setLoading] = useState(true)
+  console.log(loading)
+
+  const handleLoading=(x)=>{
+    setLoading(false, console.log(loading))
+  }
+
+
+  useEffect(()=>handleLoading(false),[])
+
   return (
     <>
-      <div id='carousel'></div>
-
-      <div className='sticky-top'  style={{height:'100vh', zIndex:'-1', maxWidth:'100%' }}>
-        <Carousel autoplay>
-          <div style={{height:'100vh', width:'100vw'}} > 
-            <div 
-              className='flex'
-              style={{
-                height:'100vh',
-                width:'100vw',
-                backgroundImage:`url('${img1}')`,
-                backgroundSize:'cover',
-                margin:'auto',
-                display:'flex',
-                justifyContent:'center',
-                alignItems:'center'
-              }}
-            >
-              <div 
-                style={{
-                  width:'70vw',
-                  height:'30vh',
-                  textAlign:'center',
-                }}
-              >
-                <ScrollAnimation
-                  animateIn='fadeInDown'
-                  // animateOut='fadeOutdown'
+      {
+        loading===true ? (<div>Loading...</div>):(
+        <>
+        <div id='carousel'></div>
+        <Spin spinning={loading}>
+          <div className='sticky-top'  style={{height:'100vh', zIndex:'-1', maxWidth:'100%'}}>
+            <Carousel autoplay>
+              <div onLoad={()=>handleLoading(false)} style={{height:'100vh', width:'100vw'}} > 
+                <div 
+                  className='flex'
+                  // onLoad={()=>handleLoading(false)}
+                  style={{
+                    height:'100vh',
+                    width:'100vw',
+                    backgroundImage:`url('${img1}')`,
+                    backgroundSize:'cover',
+                    margin:'auto',
+                    display:'flex',
+                    justifyContent:'center',
+                    alignItems:'center'
+                  }}
                 >
-                  <p 
+                  <div 
                     style={{
-                      color:'white',
-                      fontFamily:'lobster',
-                      fontSize:'12vmin',
+                      width:'70vw',
+                      height:'30vh',
+                      textAlign:'center',
                     }}
                   >
-                    Brunches Cafe
-                  </p>
-                </ScrollAnimation>
+                    <ScrollAnimation
+                      animateIn='fadeInDown'
+                      // animateOut='fadeOutdown'
+                    >
+                      <p 
+                        style={{
+                          color:'white',
+                          fontFamily:'lobster',
+                          fontSize:'12vmin',
+                        }}
+                      >
+                        Brunches Cafe
+                      </p>
+                    </ScrollAnimation>
 
-                <ScrollAnimation
-                  animateIn='fadeInDown'
-                  // animateOut='fadeOutdown'
-                >
-                  <p
-                    style={{
-                      fontFamily:'lemonada',
-                      // color:'#381c07',
-                      color:'white'
-                    }}
-                  >
-                    A Perfect Meal Everytime...
-                  </p>
-                </ScrollAnimation>
+                    <ScrollAnimation
+                      animateIn='fadeInDown'
+                      // animateOut='fadeOutdown'
+                    >
+                      <p
+                        style={{
+                          fontFamily:'lemonada',
+                          // color:'#381c07',
+                          color:'white'
+                        }}
+                      >
+                        A Perfect Meal Everytime...
+                      </p>
+                    </ScrollAnimation>
+                  </div>
+                
+                </div>
               </div>
-            
-            </div>
+            </Carousel>
           </div>
-        </Carousel>
-      </div>
+        </Spin>
+        </>
+        )
+      }
     </>
   );
 }
